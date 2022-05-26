f535d8100967d003ddae8dcbd33c50c38086397d
HuyinSong24653 <80292613+HuyinSong24653@users.noreply.github.com>
2022-05-26 13:06:16 +1200

    The beginning of my program
    Simply just the imports and such.

---
+++ Huyin Song 13B7 Programming with Processes.py
@@ -0,0 +1,18 @@
+#Huyin Song, Started on 6/05/2022
+#Improved interactivity quiz as described in Critical Inquiry Assessment
+
+from tkinter import*
+root = Tk()
+root.geometry("1250x750")
+
+ename = Entry(root)
+lname = Label(root)
+eage = Entry(root)
+lage = Label(root)
+
+
+
+
+
+
+root.mainloop()
