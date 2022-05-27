ce23fdc80549049018ab0257ca68376e2b721a46
Huyin Song <huyinsong2004@gmail.com>
2022-05-27 12:25:47 +1200

    Resized GUI window, Added labels and entry boxes, set positions for labels and entry boxes.

--- Huyin Song 13B7 Programming with Processes.py
+++ Huyin Song 13B7 Programming with Processes.py
@@ -3,19 +3,28 @@
 
 from tkinter import*
 root = Tk()
-root.geometry("1250x750")
+root.geometry("500x500")
+
+
+lname = Label(root, text = "What is your name?")
+lname.pack()
+lname.place(x = 20, y = 20)
+
+lage = Label(root, text = "What is your age?")
+lage.pack()
+lage.place(x = 20, y = 60)
 
 ename = Entry(root)
-lname = Label(root)
+ename.pack()
+ename.place(x = 150, y = 20)
+
+
 eage = Entry(root)
-lage = Label(root)
+eage.pack()
+eage.place(x = 150, y = 60)
 
 
-#SUSSY TESTING
-#TESTING TESTING
 
-
-#TESTING AGAIN
 
 
 root.mainloop()
