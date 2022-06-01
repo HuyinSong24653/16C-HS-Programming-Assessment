31d9ce9bdfcb30e993c13ee7661871ce0d6a9f1f
Huyin Song <huyinsong2004@gmail.com>
2022-06-01 12:45:20 +1200

    Added dev button to easily test, couldn't find a way to build GUI outside of function, started buttons and labels in option select gui

--- Huyin Song 13B7 Programming with Processes.py
+++ Huyin Song 13B7 Programming with Processes.py
@@ -1,12 +1,13 @@
 #Huyin Song, Started on 6/05/2022
 #Improved interactivity quiz as described in Critical Inquiry Assessment
 
-import tkinter as tk
+from tkinter import*
 
-verification = tk.Tk()
+verification = Tk()
+verification.title("Verifications window")
 verification.geometry("350x350")
 
-lbl_verifage = tk.Label(verification)
+lbl_verifage = Label(verification)
 lbl_verifage.place(x = 20, y = 200)
 
 def verifyage():
@@ -15,31 +16,52 @@ def verifyage():
     if userage >= 16:
         lbl_verifage.config(text = "You are qualified to use my program!")
         btn_proceed.place(x = 100, y = 275)
-        lbl_welcome = tk.Label(verification, text = "{}, Welcome!".format(username))
+        lbl_welcome = Label(verification, text = "{}, Welcome!".format(username))
         lbl_welcome.place(x = 20, y = 160)
     elif userage < 16:
         lbl_verifage.config(text = "You are not old enough to qualify")
 
-def quizselect():
-    quizselect = tk.Tk()
-    quizselect.geometry("350x350")
+def selection():
+    quizselect = Tk()
+    quizselect.title("Quiz topic selecter")
+    quizselect.geometry("500x450")
     verification.destroy()
+    lbl_quizselectwelcome = Label(quizselect, text = "What topic would you like to be tested on?")
+    lbl_quizselectwelcome.place(x = 125, y = 20)
+    btn_general = Button(quizselect, text = "General")
+    btn_general.place(x = 75, y = 100)
+    btn_emergencies = Button(quizselect, text = "Emergencies")
+    btn_emergencies.place(x = 175, y = 100)
+    btn_parking = Button(quizselect, text = "Parking")
+    btn_parking.place(x = 75, y = 150)
+    btn_road = Button(quizselect, text = "Road")
+    btn_road.place(x = 175, y = 150)
+    btn_rules = Button(quizselect, text = "Rules")
+    btn_rules.place(x = 75, y = 200)
+    btn_driver = Button(quizselect, text = "Driver")
+    btn_driver.place(x = 175, y = 200)
 
-lbl_name = tk.Label(verification, text = "What is your name?")
+lbl_name = Label(verification, text = "What is your name?")
 lbl_name.place(x = 20, y = 20)
 
-lbl_age = tk.Label(verification, text = "What is your age?")
+lbl_age = Label(verification, text = "What is your age?")
 lbl_age.place(x = 20, y = 60)
 
-ent_name = tk.Entry(verification)
+ent_name = Entry(verification)
 ent_name.place(x = 150, y = 20)
 
-ent_age = tk.Entry(verification)
+ent_age = Entry(verification)
 ent_age.place(x = 150, y = 60)
 
-btn_age = tk.Button(verification, text = "Verify your age!", bg = "#67fcd0", command = verifyage)
+btn_age = Button(verification, text = "Verify your age!", bg = "#67fcd0", command = verifyage)
 btn_age.place(x = 100, y = 100)
 
-btn_proceed = tk.Button(verification, text = "Proceed with program", bg = "#67fcd0", command = quizselect)
+btn_proceed = Button(verification, text = "Proceed with program", bg = "#67fcd0", command = selection)
+
+
+#DEVELOPER SHORTCUT
+btn_dev = Button(verification, text = "dev", bg = "#67fcd0", command = selection)
+btn_dev.pack()
+
 
 verification.mainloop()
